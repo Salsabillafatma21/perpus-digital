c3a0d627417065b850751f4dd96e009d59e25840
Salsabillafatma21 <222310280@smkn4-tsm.sch.id>
2024-03-25 14:37:48 +0700

    first commit

---
+++ components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="row p-3 bg-primary text-white">
+    <div class="col=lg=1">
+      <img src="-/assets/img/LOGO-smk4.png" alt="logo SMKN4":>
+    </div>
+
+    <div class="col-lg-11">
+      <h1>Perpus digital</h1>
+      <address>
+        SMKN 4 TASIKMALAYA
+        JL.DEPOK,SUKAMAJU,PURBARATU
+      </address>
+    </div>
+  </div>
+</template>
