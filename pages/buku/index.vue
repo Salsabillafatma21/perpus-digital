18452915558a568a06b4d276a084d7b26edb85cd
SALSABILLA FATMA DIANA <141709254+Salsabillafatma21@users.noreply.github.com>
2024-07-30 14:00:21 +0700

    Add files via upload

--- pages/buku/index.vue
+++ pages/buku/index.vue
@@ -4,36 +4,36 @@
             <div class="col-lg-12">
                 <h2 class="text-center my-4" style="color: aliceblue;">RAK BUKU</h2>
                 <div class="my-3 d-flex justify-content-center">
-                    <input type="search" class="form-control rounded-5 cari" placeholder="SEARCH?..." style="background-color: #B0CFE5;">
+                    <input type="search" class="form-control rounded-5 cari" placeholder="SEARCH?..." style="background-color: #6C73B2;">
                 </div>
                 <div class="my-3 text-muted"></div>
                 <div class="row layer m-5 rounded-5">
-                    <h1 class="text-center" style="color: aliceblue;">Koleksi Buku</h1>
+                    <h1 class="text-center" style="color: #6C73B2">Koleksi Buku</h1>
                     <div class="col-lg-2">
                         <div class="card mb-3">
                             <div class="card-body">
-                                <img src="~/assets/img/k.jpeg" class="cover" alt="cover 1" style="width: 100%;">
+                                <img src="../assets/img/k.jpeg" class="cover" alt="cover 1" style="width: 100%;">
                             </div>
                         </div>
                     </div>
                     <div class="col-lg-2">
                         <div class="card mb-3">
                             <div class="card-body">
-                                <img src="~/assets/img/p.jpeg" class="cover" alt="cover 2" style="width: 100%;">
+                                <img src="../assets/img/p.jpeg" class="cover" alt="cover 2" style="width: 100%;">
                             </div>
                         </div>
                     </div>
                     <div class="col-lg-2">
                         <div class="card mb-3">
                             <div class="card-body">
-                                <img src="~/assets/img/n.jpeg" class="cover" alt="cover 3" style="width: 100%;">
+                                <img src="../assets/img/n.jpeg" class="cover" alt="cover 3" style="width: 100%;">
                             </div>
                         </div>
                     </div>
                     <div class="col-lg-2">
                         <div class="card mb-3">
                             <div class="card-body">
-                                <img src="~/assets/img/h.jpeg" class="cover" alt="cover 3" style="width: 100%;">
+                                <img src="../assets/img/h.jpeg" class="cover" alt="cover 3" style="width: 100%;">
                             </div>
                         </div>
                     </div>
@@ -45,8 +45,9 @@
 <style scoped>
 
 .content {
-    background-color: #658694;
-}
+    background-color: #C49DC0;
+};
+
 
 .cari{
     width: 40rem;
@@ -56,6 +57,6 @@
     width: 50%;
 } */
 .layer {
-    background-color: #89B6D6;
+    background-color: #6C73B2;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
         <div class="container-fluid">
-            <div class="row py-5">
-                <div class="col-lg-6">
+            <div class="row 2 py-5">
+                <div class="col-lg-8">
                     <nuxt-link to="/pengunjung/tambah">
                         <div class="card bg-pengunjung rounded-5">
                             <div class="card-body">
@@ -10,40 +10,19 @@
                             </div>
                         </div>
                     </nuxt-link>
+                </div>
             </div>
-            <div class="col-lg-6">
-                <nuxt-link to="/buku">
-                    <div class="card bg-buku rounded-5">
-                        <div class="card-body">
-                            <h2>cari buku</h2>
+            <div class="row justify-content-end">
+                <div class="col-lg-8">
+                    <nuxt-link to="/buku">
+                        <div class="card bg-buku rounded-5">
+                            <div class="card-body">
+                                <h2>cari buku</h2>
+                            </div>
                         </div>
-                    </div>
-                </nuxt-link>
-        </div>
-        <h2>Statistik</h2>
-        <div class="row py-5">
-            <nuxt-link to="/pengunjung/index"></nuxt-link>
-            <div class="col-lg-6">
-                <nuxt-link to="/pengunjung/index">
-                    <div class="card pengunjung rounded-5">
-                        <div class="card-body">
-                            <h2>Isi buku kunjungan</h2>
-                        </div>
-                    </div>
-                </nuxt-link>
+                    </nuxt-link>
+                </div>
             </div>
-
-            <div class="col-lg-6">
-                <nuxt-link to="/buku">
-                    <div class="card buku rounded-5">
-                        <div class="card-body">
-                            <h2>50 Buku</h2>
-                        </div>
-                    </div>
-                </nuxt-link>
-            </div>
-        </div>
-    </div>
         </div>
     </div>
 </template>
@@ -52,23 +31,24 @@
 .container-fluid {
     /* padding: 0;
     margin: 0; */
-    background-color: #353D84;
+    background-color: #C49DC0;
 }
 
 .card {
     height: 250px;
-    box-shadow: 1px 1px 10px hsl(0, 0%, 100%)
+    background-color: #353D84;
+    box-shadow: 1px 1px 10px (0, 0%, 100%)
 }
 
 .card.bg-pengunjung {
-    background-image: url('../assets/img/pengunjung.jpeg');
+    background-image: url('../assets/pengunjung.jpeg');
     background-repeat: no-repeat;
     background-position: center center;
     background-size: cover;
 }
 
 .card.bg-buku {
-    background: url('../assets/img/cari buku.jpeg') no-repeat center center;
+    background: url('../assets/caribuku.jpg') no-repeat center center;
     background-size: cover;
 }
 
@@ -77,6 +57,6 @@
 }
 
 .buku {
-    background-color: #C49DC0;
+    background-color: #353D84;
 }
 </style>
